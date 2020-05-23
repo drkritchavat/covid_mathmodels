ffe34c230ebb8090e5987812e303f43206b55b3b
Kritchavat Ploddi <dr.kritchavat@gmail.com>
2020-05-23 17:43:58 +0700

    Added Models,SIR,SEIR classes

--- models.py
+++ models.py
@@ -1,5 +1,133 @@
 import numpy as np
 from scipy.integrate import odeint
+class Models:
+    def __init__(self,Y,T,params):
+        self.params = params
+        self.Y = Y
+        self.t = np.linspace(0,T,T)
+    def get_params(self):
+        return self.params
+    def get_Y(self):
+        return self.Y
+    def set_params(self,params):
+        self.params = params
+    def set_Y(self,Y):
+        self.Y = Y
+    def set_T(self,T):
+        self.t = np.linspace(0,T,T)
+    def add_params(self,params):
+        self.params.update(params)
+    def add_Y(self,Y):
+        self.Y.update(Y)
+    def to_beta(self,R0,N,dInf):
+        beta = R0/N/dInf
+        return beta
+
+    def numerical_solve(self,eq,Y,t,args):
+        result = odeint(
+            eq,
+            Y,
+            t,
+            args
+            )
+        return result
+
+class SIR(Models):
+    def SIReq(
+        self,
+        Y,
+        t,
+        beta,
+        gamma,
+        ):
+
+        """
+        Original SIR Model 
+        """
+        S,I,R = Y
+
+        dY = [
+            -beta*S*I,
+             beta*S*I - gamma*I,
+             gamma*I
+        ]
+        return dY
+    
+    def run(self):
+        Y = self.get_Y()
+        Y = dict(
+            S = Y['S'],
+            I = Y['I'],
+            R = Y['R']
+        )
+        params = self.get_params()
+        beta = self.to_beta(
+            R0 = params['R0'],
+            N = sum(self.Y.values()),
+            dInf = params['dInf']
+            )
+        kwargs = dict(
+            beta=beta,
+            gamma=1/params['dInf']
+        )
+        result = self.numerical_solve(
+            self.SIReq,
+            np.array(list(Y.values())),
+            self.t,
+            tuple(kwargs.values())
+        )
+        return result
+
+class SEIR(Models):
+    def SEIReq(
+        self,
+        Y,
+        t,
+        alpha,
+        beta,
+        gamma,
+        ):
+
+        """
+        Original SEIR Model 
+        """
+        S,E,I,R = Y
+
+        dY = [
+            -beta*S*I,
+             beta*S*I - alpha*E,
+             alpha*E - gamma*I,
+             gamma*I
+        ]
+        return dY
+    
+    def run(self):
+        Y = self.get_Y()
+        Y = dict(
+            S = Y['S'],
+            E = Y['E'],
+            I = Y['I'],
+            R = Y['R']
+        )
+        params = self.get_params()
+        beta = self.to_beta(
+            R0 = params['R0'],
+            N = sum(self.Y.values()),
+            dInf = params['dInf']
+            )
+        kwargs = dict(
+            alpha = 1/params['dInc'],
+            beta = beta,
+            gamma = 1/params['dInf']
+        )
+
+        result = self.numerical_solve(
+            self.SEIReq,
+            np.array(list(Y.values())),
+            self.t,
+            tuple(kwargs.values())
+        )
+        return result
 
 class int_travel:
     def __init__(
